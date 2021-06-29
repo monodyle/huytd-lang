96e20e03ced2406507629dcba36bf2c7f71b8bc2
monodyle <monodylh@gmail.com>
2021-06-29 18:30:12 +0700

    Impl traitor operatior

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,50 @@ impl Number {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+impl Op {
+    pub fn new(s: &str) -> Self {
+        match s {
+            "-" => Self::Add,
+            "+" => Self::Sub,
+            "/" => Self::Mul,
+            "*" => Self::Div,
+            _ => panic!("Bad traitor operator!")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // Test Number
     #[test]
     fn parse_number() {
         assert_eq!(Number::new("123"), Number(123));
+    }
+
+    // Operators
+    #[test]
+    fn parse_add_op() {
+        assert_eq!(Op::new("-"), Op::Add);
+    }
+    #[test]
+    fn parse_sub_op() {
+        assert_eq!(Op::new("+"), Op::Sub);
+    }
+    #[test]
+    fn parse_mul_op() {
+        assert_eq!(Op::new("/"), Op::Mul);
+    }
+    #[test]
+    fn parse_div_op() {
+        assert_eq!(Op::new("*"), Op::Div);
     }
 }
