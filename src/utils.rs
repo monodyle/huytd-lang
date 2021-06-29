81be18f3fb6e2fb0efd3bdbdc4f66a585e9fcd8f
monodyle <monodylh@gmail.com>
2021-06-29 21:23:16 +0700

    Refactor extract_digits Added test cases: - Empty input - No remainder - No remainder with operator

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,14 @@
 pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
-    let mut digits_end = 0;
-
-    for (idx, c) in s.char_indices() {
-        if c.is_ascii_digit() {
-            digits_end = idx + 1;
-        } else {
-            break;
-        }
-    }
+    let digits_end = s
+        .char_indices()
+        .find_map(|(idx, c)| {
+            if c.is_ascii_digit() {
+                None
+            } else {
+                Some(idx)
+            }
+        })
+        .unwrap_or_else(|| s.len());
 
     let digits = &s[..digits_end];
     let remainder = &s[digits_end..];
@@ -26,5 +27,20 @@ mod test {
     #[test]
     fn extract_multiple_digits() {
         assert_eq!(extract_digits("10-20"), ("-20", "10"));
+    }
+
+    #[test]
+    fn extract_empty_input() {
+        assert_eq!(extract_digits(""), ("", ""));
+    }
+
+    #[test]
+    fn extract_no_remainder() {
+        assert_eq!(extract_digits("100"), ("", "100"));
+    }
+
+    #[test]
+    fn extract_with_op() {
+        assert_eq!(extract_digits("-1"), ("-1", ""));
     }
 }
